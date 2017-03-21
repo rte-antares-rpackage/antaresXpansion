e34d59ebe29d405f65a5457d6e3f047760e5ec71
Baptiste <baptiste.seguinot@rte-france.com>
2017-03-21 17:38:54 +0100

    Amend to previous commit

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+# Define global variables to avoid warnings while checking the package
+
+globalVariables(c("theta", 
+                  "timeId", 
+                  "link", 
+                  "mcYear", 
+                  "it", 
+                  "theta_previous", 
+                  "it.x", 
+                  "weight", 
+                  "sum_over_week", 
+                  "sum_over_year"))
