bb96e57606ddbce8eed4dfba8173e678e7a8e16c
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2018-05-16 13:54:18 +0200

    update get_op_costs() so as to handle yearly weights

--- R/get_op_costs.R
+++ R/get_op_costs.R
@@ -11,7 +11,8 @@
 #' @return 
 #' Returns the operationnal cost of the given study 
 #' 
-#' @importFrom antaresRead readAntares 
+#' @importFrom antaresRead readAntares
+#' @importFrom assertthat assert_that 
 #' @noRd
 get_op_costs <- function(output_antares, current_it, exp_options)
 {
@@ -28,17 +29,41 @@ get_op_costs <- function(output_antares, current_it, exp_options)
     {
       # in that case, non-linear cost has to be removed because they are computed in a post-processing and are not
       # part of the ANTARES optimization
-      output_area_s <- antaresRead::readAntares(areas = "all", links = NULL, mcYears = NULL, 
+      if(all(is.na(exp_options$y_weights)))
+      {
+        # if Monte-Carlo years are equally weighted, synthetic data are loaded
+        output_area_s <- antaresRead::readAntares(areas = "all", links = NULL, mcYears = NULL, 
                                                 timeStep = "annual", opts = output_antares, showProgress = FALSE,
                                                 select = c("OV. COST", "NP COST"))
       
-      output_link_s <- antaresRead::readAntares(areas = NULL, links = "all", mcYears = NULL, 
+        output_link_s <- antaresRead::readAntares(areas = NULL, links = "all", mcYears = NULL, 
                                                 timeStep = "annual", opts = output_antares, showProgress = FALSE,
                                                 select = c("HURDLE COST"))
       
       
-      op_cost <-  sum(as.numeric(output_area_s$"OV. COST"))  + sum(as.numeric(output_link_s$"HURDLE COST")) -
+        op_cost <-  sum(as.numeric(output_area_s$"OV. COST"))  + sum(as.numeric(output_link_s$"HURDLE COST")) -
         sum(as.numeric(output_area_s$"NP COST"))
+      }
+      else
+      {
+        # if Monte-Carlo years are not equally weighted
+        output_area_y <- antaresRead::readAntares(areas = "all", links = NULL, mcYears = "all", 
+                                                  timeStep = "annual", opts = output_antares, showProgress = FALSE,
+                                                  select = c("OV. COST", "NP COST"))
+        
+        output_link_y <- antaresRead::readAntares(areas = NULL, links = "all", mcYears = "all", 
+                                                  timeStep = "annual", opts = output_antares, showProgress = FALSE,
+                                                  select = c("HURDLE COST"))
+        
+        
+        op_cost_y <- sapply(output_antares$mcYears,
+                          FUN = function(n){ sum(as.numeric(output_area_y[mcYear ==n]$"OV. COST"))+ 
+                                             sum(as.numeric(output_link_y[mcYear ==n]$"HURDLE COST")) -
+                                             sum(as.numeric(output_area_y[mcYear ==n]$"NP COST"))})
+        
+        # works only in case of full iteration
+        op_cost <- sum(op_cost_y * exp_options$y_weights)
+      }
     }
     else if (exp_options$uc_type == "expansion_accurate")
     {
@@ -51,16 +76,18 @@ get_op_costs <- function(output_antares, current_it, exp_options)
     {
       # that case should no longer be used (old fast and accurate mode, which are not relaxed
       # and therefore not fitted for a benders decomposition)
-      output_area_s <- antaresRead::readAntares(areas = "all", links = NULL, mcYears = NULL, 
-                                                timeStep = "annual", opts = output_antares, showProgress = FALSE,
-                                                select = "OV. COST")
+      assertthat::assert_that(TRUE)
       
-      output_link_s <- antaresRead::readAntares(areas = NULL, links = "all", mcYears = NULL, 
-                                                timeStep = "annual", opts = output_antares, showProgress = FALSE,
-                                                select = c("HURDLE COST"))
-      
-      
-      op_cost <-  sum(as.numeric(output_area_s$"OV. COST"))  + sum(as.numeric(output_link_s$"HURDLE COST")) 
+      # output_area_s <- antaresRead::readAntares(areas = "all", links = NULL, mcYears = NULL, 
+      #                                           timeStep = "annual", opts = output_antares, showProgress = FALSE,
+      #                                           select = "OV. COST")
+      # 
+      # output_link_s <- antaresRead::readAntares(areas = NULL, links = "all", mcYears = NULL, 
+      #                                           timeStep = "annual", opts = output_antares, showProgress = FALSE,
+      #                                           select = c("HURDLE COST"))
+      # 
+      # 
+      # op_cost <-  sum(as.numeric(output_area_s$"OV. COST"))  + sum(as.numeric(output_link_s$"HURDLE COST")) 
     }
   }
   return(op_cost)
