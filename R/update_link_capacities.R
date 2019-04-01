0473b7fab9ce5f5caa05f6a434cb3f7803e636ad
Baptiste Seguinot <baptiste.seguinot@rte-france.com>
2019-04-01 14:36:56 +0200

    update_link_capacities() handles several candidates on the same link

--- R/update_link_capacities.R
+++ R/update_link_capacities.R
@@ -26,24 +26,33 @@
 #' 
 update_link_capacities <- function(x, current_it, candidates, exp_options, opts)
 {
-  for(c in candidates)
+  
+  links_with_candidates <- sapply(candidates, FUN = function(c){c$link})
+
+  for(l in links_with_candidates)
   {
+    id_list <- which(links_with_candidates == l)
+    new_capacity_direct <- candidates[[id_list[1]]]$already_installed_capacity*candidates[[id_list[1]]]$already_installed_link_profile
+    new_capacity_indirect <- candidates[[id_list[1]]]$already_installed_capacity*candidates[[id_list[1]]]$already_installed_link_profile_indirect
     
-    new_capacity_direct   <- get_capacity_profile(get_capacity(x$invested_capacities, candidate = c$name, it = current_it$n),
-                                                  c$link_profile, exp_options$uc_type)+c$already_installed_capacity*c$already_installed_link_profile
+    for(id in id_list)
+    {
+      new_capacity_direct <-  new_capacity_direct + get_capacity_profile(get_capacity(x$invested_capacities, candidate = candidates[[id]]$name, it = current_it$n),
+                                                                         candidates[[id]]$link_profile, exp_options$uc_type)
+        
     
-    new_capacity_indirect <- get_capacity_profile(get_capacity(x$invested_capacities, candidate = c$name, it = current_it$n),
-                                                  c$link_profile_indirect, exp_options$uc_type)+c$already_installed_capacity*c$already_installed_link_profile_indirect
+      new_capacity_indirect <- new_capacity_indirect + get_capacity_profile(get_capacity(x$invested_capacities, candidate = candidates[[id]]$name, it = current_it$n),
+                                                                            candidates[[id]]$link_profile_indirect, exp_options$uc_type)
+    }
     
-    if(c$has_link_profile_indirect)
+    if(candidates[[id_list[1]]]$has_link_profile_indirect)
     { 
       new_capacity_direct[new_capacity_direct < 1 ] <- 1
       new_capacity_indirect[new_capacity_indirect < 1 ] <- 1
     }
     
     # update study
-    update_link(c$link, "direct_capacity", new_capacity_direct , opts)
-    update_link(c$link, "indirect_capacity", new_capacity_indirect, opts)
+    update_link(l, "direct_capacity", new_capacity_direct , opts)
+    update_link(l, "indirect_capacity", new_capacity_indirect, opts)
   }
-  
 }
